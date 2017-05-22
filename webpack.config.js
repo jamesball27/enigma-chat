3f70cad0a655d8454c11fc9ac8a00b3257a0a40f
jamesball27 <jwrb27@gmail.com>
2017-05-22 17:33:45 -0400

    Initial commit, configure webpack and node modules

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
